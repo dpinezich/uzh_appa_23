d5a414ec41d5ec1fd0d9077875d667f14a43bc93
dpinezich <david.pinezich@gmail.com>
2024-01-18 18:23:22 +0100

    callback missing

--- data-automation/02-examples/16_dash_with_data_and_vis_cont.py
+++ data-automation/02-examples/16_dash_with_data_and_vis_cont.py
@@ -18,6 +18,14 @@ app.layout = html.Div([
     dcc.Graph(figure={}, id='controls-and-graph')
 ])
 
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
 # Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5006)
+    app.run(host='0.0.0.0', port=5088)
